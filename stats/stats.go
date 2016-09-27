4e340bee04dc841bbdc98266de87adedac6b1f6c
Bastien <basgys@gmail.com>
2016-09-27 15:13:57 +0200

    Document stats package

--- stats/stats.go
+++ stats/stats.go
@@ -11,11 +11,20 @@ type Stats interface {
 	Start()
 	Stop()
 
+	// Count is a simple counter
 	Count(key string, n interface{}, meta ...map[string]string)
+	// Inc increments the given counter by 1
 	Inc(key string, meta ...map[string]string)
+	// Dec decrements the given counter by 1
 	Dec(key string, meta ...map[string]string)
+	// Gauge measures the amount, level, or contents of something
+	// The given value replaces the current one
+	// e.g. in-flight requests, uptime, ...
 	Gauge(key string, n interface{}, meta ...map[string]string)
+	// Timing measures how long it takes to accomplish something
+	// e.g. algorithm, request, ...
 	Timing(key string, t time.Duration, meta ...map[string]string)
+	// Histogram measures the distribution of values over the time
 	Histogram(key string, n interface{}, tags ...map[string]string)
 
 	SetLogger(l log.Logger)
